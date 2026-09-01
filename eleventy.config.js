40cad001a5a2f3a8c504dfd3d4843068d861462d
Daniel <champion327@gmail.com>
2026-08-31 19:09:31 -0600

    Add bilingual support, favicon, SEO metadata, and TL;DR toggle view

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy("cauldron.png");
 
   return {
     dir: {
